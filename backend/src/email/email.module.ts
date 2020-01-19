ba4d81f4af0fcace279e5a91d36e4f1bbd67d981
Nick Mitchell <nick@kitset.io>
2020-01-20 10:39:59 +1100

    improvement(emailservice): export email service globally
    so that it can be used elsewhere in the app

--- backend/src/email/email.module.ts
+++ backend/src/email/email.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
 
+@Global()
 @Module({
   controllers: [EmailController],
-  providers: [EmailService]
+  providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
