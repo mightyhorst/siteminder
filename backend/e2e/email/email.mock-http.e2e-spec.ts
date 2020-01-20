200805f10e8537ec5c4c19d166da7b0784d09b5d
Nick Mitchell <nick@kitset.io>
2020-01-20 20:17:21 +1100

    test(email.e2e): add email e2e tests
    add specs for email api e2e mocking the emailservice and the http service respectively
    
    closes #10

---
+++ backend/e2e/email/email.mock-http.e2e-spec.ts
@@ -0,0 +1,94 @@
+import * as supertestRequest from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication, HttpService, HttpModule } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
+
+import { EmailModule } from '../../src/email/email.module';
+import { EmailService } from '../../src/email/email.service';
+import { mockBody } from './mock.email.service';
+
+const axiosResponse: AxiosResponse = {
+    status: 202,
+    statusText: 'Accepted',
+    headers: {},
+    config: {},
+    data: {}
+}
+
+
+
+describe('Email API (mock http service)', () => {
+    let app: INestApplication;
+    let httpService: HttpService;
+    let mockHttpService = {
+        post: jest.fn((
+            sendgridUrl, 
+            postData, 
+            axiosConfig
+        )=> of(axiosResponse))
+    }
+
+    /**
+     * @name beforeAll setup the testing module 
+     */
+    beforeAll(async () => {
+
+        const module = await Test.createTestingModule({
+            imports: [EmailModule, HttpModule]
+        })
+            .overrideProvider(HttpService)
+            .useValue(mockHttpService)
+            .compile();
+
+        app = module.createNestApplication();
+        httpService = module.get<HttpService>(HttpService);
+        await app.init();
+    });
+
+
+    /** 
+     * @name POST email send 
+     * @description send an email and expect the http service to be called 
+     */
+    it(`/POST email/send`, async (done) => {
+        
+        
+        /*
+        const spy = jest.spyOn(httpService, 'post')
+        spy.mockImplementationOnce((
+            sendgridUrl, 
+            postData, 
+            axiosConfig
+        )=> of(axiosResponse));
+        */
+
+        /**
+         * @param {SuperTestRequest} supertestRequest - axios mock for supertest 
+         */
+        const supertestResponse = await supertestRequest(app.getHttpServer())
+            .post('/email/send')
+            .send(mockBody)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(202)
+            .expect({
+                "status": 202,
+                "statusText": "Accepted",
+                "data": {}
+            });
+
+        expect(mockHttpService.post).toHaveBeenCalled();
+
+        done();
+
+        return supertestResponse;
+    });
+
+    /**
+     * @name afterAll close the app 
+     */
+    afterAll(async () => {
+        await app.close();
+    });
+});
---
+++ backend/e2e/email/mock.email.service.ts
@@ -0,0 +1,48 @@
+import {Observable, of} from 'rxjs';
+import {IEmail, IEmailResponse} from '../../src/email/interfaces/index';
+
+export const mockBody = {
+    "recipients": [
+        {
+            "email": "nickymitch@gmail.com",
+            "name": "Nick Mitchell"
+        }
+    ],
+    "header": "Header",
+    "subHeader": "Sub Header",
+    "body": "Hello world ",
+    "imageUrl": "https://www.siteminder.com/wp-content/uploads/2020/01/home-page-hotel-tech-awards.png",
+    "button": "Click me",
+    "buttonUrl": "https://www.siteminder.com/",
+    "subject": "Hello, World!",
+    "sender": {
+        "email": "nickymitch@gmail.com",
+        "name": "Nick Mitchell"
+    }
+}
+
+export const mockEmailResponse:IEmailResponse = {
+    "status": 202,
+    "statusText": "Accepted",
+    "data": ""
+
+}
+export class MockEmailService{
+
+    sendWelcomeEmail(
+        recipients: IEmail[],
+        sender: IEmail,
+        subject: string,
+
+        header?: string,
+        subHeader?: string,
+        body?: string,
+        imageUrl?: string,
+        button?: string,
+        buttonUrl?: string,
+    ): Observable<IEmailResponse>
+    {
+        return of(mockEmailResponse);
+    }
+
+}
